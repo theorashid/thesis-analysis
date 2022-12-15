00b268d6e79dfbeaab68213ffe9540177b7c857c
theorashid <theoaorashid@gmail.com>
2022-12-15 13:28:57 +0000

    cancer/cause nqx script

---
+++ thesis_analysis/cancer/nqx.R
@@ -0,0 +1,25 @@
+"Convert mx rates to nqx
+"
+
+nqx <- function(age, mx, ax, n, x) {
+  mx <- mx[!is.na(age)]
+  width = c(diff(age), Inf)
+  ax[is.na(ax)] <- width/2
+  mx[mx == 0] <- 1e-10
+  
+  idx <- age >= x & age <= x + n
+  ax <- ax[idx]
+  width <- width[idx]
+  mx <- mx[idx]
+  
+  qx <- width * mx / (1 + (width - ax) * mx)
+  
+  qx[qx > 1] <- 0.99999
+  
+  px <- 1 - qx
+  
+  px <- matrix(px, nrow = length(unique(age[idx])))
+  
+  return(1 - apply(px, MARGIN = 2, FUN = prod))
+}
+
