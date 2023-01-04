0e7c7d52de17b9628a343a44fcc5830f0e946723
theorashid <theoaorashid@gmail.com>
2023-01-04 18:44:53 +0000

    update the agg xarray stuff

--- thesis_analysis/england/mx_agg.py
+++ thesis_analysis/england/mx_agg.py
@@ -19,7 +19,7 @@ if __name__ == "main":
         / "{}_{}_{}.nc".format(args.region, args.sex, args.model),
         engine="h5netcdf",
     )
-    ds["__xarray_dataarray_variable__"].rename("death_rates")
+    ds = ds.rename({"__xarray_dataarray_variable__": "death_rates"})
 
     lookup = pd.read_csv(
         p / "data" / "covariates" / "LSOA_MSOA_LAD_GOR_CTRY_lookup.csv"
@@ -30,7 +30,7 @@ if __name__ == "main":
         .drop_duplicates()
     )
 
-    lookup.set_index(["MSOA"]).to_xarray()["LAD"]
+    ds["LAD"] = lookup.set_index(["MSOA"]).to_xarray()["LAD"]
 
     population = xr.open_dataset(
         p / "data" / "population" / "{}_{}_population.nc".format(args.region, args.sex),
